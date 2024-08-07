0c1f16d09ff568adf131c281386531b7f751c760
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-08-07 23:38:55 +0300

    made the reception page

---
+++ src/pages/Reception/OpitonList.jsx
@@ -0,0 +1,18 @@
+import Option from './Option.jsx';
+import addPatient from '../../images/addpatient.png';
+import patientList from '../../images/listPatient.png'
+import addBill from '../../images/addinvoice.png'
+import bills from '../../images/invoice.png'
+
+const OptionList = () => {
+  return (
+    <div className=" grid grid-cols-2 p-4 place-items-center gap-4">
+      <Option imgLink={addPatient} optionText={'اضافة مريض'} />
+      <Option imgLink={patientList} optionText={"قائمة المرضى"} />
+      <Option imgLink={addBill} optionText={'اضافة فاتورة'} />
+      <Option imgLink={bills} optionText={'الفواتير'} />
+    </div>
+  );
+};
+
+export default OptionList;
---
+++ src/pages/Reception/Option.jsx
@@ -0,0 +1,11 @@
+const Option = ({ imgLink, optionText }) => {
+  return (
+    <div className=" bg-white w-[400px] h-[200px] grid place-items-center rounded-lg relative group overflow-hidden hover:bg-[#E1F4F4] cursor-pointer z-0">
+      <img src={imgLink} alt="" className="w-[70px] h-[70px]" />
+      <h2 className="font-bold text-2xl">{optionText} </h2>
+      <div className="bg-blue-500 absolute w-[50px] h-[50px] top-0 left-0 rounded-bl-md rounded-br-full opacity-0 group-hover:opacity-100 transition 0.3s"></div>
+    </div>
+  );
+};
+
+export default Option;
