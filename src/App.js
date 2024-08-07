df715a5e40928359b08d25e77d86cf35245c6908
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-08-07 20:25:23 +0300

    changed the font from mono to amiri

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Homepage from '../src/pages/Homepage/home'
 function App() {
   return (
-    <div className="App mx-auto">
+    <div className="App mx-auto font-amiri">
      <Homepage />  
     </div>  
   );  
