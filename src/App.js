98a7a111190e26abe7fb734d340791ae757301a0
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-08-10 12:27:43 +0300

    changes to the branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
-import Homepage from '../src/pages/Homepage/home'
+import Homepage from '../src/pages/Homepage/home';
+import Reception from './pages/Reception/Reception';
 function App() {
   return (
     <div className="App mx-auto font-amiri">
-     <Homepage />  
-    </div>  
-  );  
+      <Reception />
+    </div>
+  );
 }
- 
+
 export default App;
