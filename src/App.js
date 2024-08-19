236aa2b2f171d743c2ad4b61ad05b9f02e63b3a7
engbaqer <alkbaqer@gmail.com>
2024-08-19 16:00:51 +0300

    add cardpage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Homepage from '../src/pages/Homepage/home'
 import InvoicePageb from './pages/InvoicePage_b/InvoicePage'
+import Cardpage from './pages/Cardpage/Cardpage';
 function App() {
   return (
-    <div className="App mx-auto">
-     <InvoicePageb />  
+    <div className="App mx-auto font-amiri">
+     < Cardpage/>  
     </div>  
+    
   );  
 }
  
