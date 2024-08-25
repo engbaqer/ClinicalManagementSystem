08711e8ac84bb5f75899407a89c60fdba2f2fd43
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-08-25 21:32:02 +0300

    made new navigation list to dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import Welcome from './Welcome';
 const Dashboard = () => {
   return (
     <div className="h-screen w-full pb-10">
+      {/* header component */}
       <DashBoardHeader />
       {/* main content */}
       <div className="2xl:w-[70%] w-[80%] m-auto h-screen">
