8a6329ae156aa5fe041d77ebb8acfafdc398e13e
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-15 17:24:09 +0300

    added data fetching to requests page

--- src/pages/DrugStore/RequestPage/RequestsPage.jsx
+++ src/pages/DrugStore/RequestPage/RequestsPage.jsx
@@ -2,12 +2,39 @@ import FilterRequests from "./FilterRequests";
 import RequestHeader from "./RequestsHeader";
 import Request from "./Requests";
 import fake_data from './fake-requests.json'
+import { useContext, useState, useEffect} from "react";
+import { ClinicalContext } from "../../auth/contextFile";
+import axios from "axios";
+
 const RequestPage = () => {
+  const { token } = useContext(ClinicalContext);
+  const [pharmacyRequests, setPharmacyRequests] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const respone = await axios.get(
+          'http://localhost:4000/api/pharmacist/reqList',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = respone.data;
+        setPharmacyRequests(data);
+      } catch (err) {
+        console.error(err)
+      }
+    })();
+  }, []);
+
   return (
     <div className="w-[85%] mx-auto mb-4">
       <RequestHeader />
       <FilterRequests />
-      {fake_data.map((request, index) => {
+      {pharmacyRequests.map((request, index) => {
         return <Request requestDetails={request} />
       })}
     </div>
