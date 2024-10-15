bce35fdb22c48a9bec83a7f2e3870dd93be42c38
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-15 17:23:22 +0300

    added request response from drug store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Login from './pages/Login/Login';
-
 import Homepage from '../src/pages/Homepage/home';
 import RPL from './pages/Receptionist-Patient-List/RpatientList';
 import RP_AddPatient from './pages/RP_AddPatient/RP_AddPatient';
@@ -26,6 +25,7 @@ import AddProduct from './pages/AddProduct/AddProduct';
 import Addprescription from './pages/AddPrescription/AddPrescription'
 import ProductAndPriceData from './pages/ProductAndPriceData/ProductAndPriceData'
 import TheInvoice from './pages/theInvoice/theInvoice';
+import ResponsePage from './pages/DrugStore/RequestResponse/ResponsePage';
 function App() {
   return (
     <div className="App mx-auto font-amiri">
@@ -34,11 +34,11 @@ function App() {
 
           <Route path="/Login" element={<Login />} />
           <Route element={<Auth />} >
-           <Route path="/Addprescription" element={<Addprescription />} />
-          <Route path="/AddProduct" element={<AddProduct />} />
-          <Route path="/theInvoice/:ivoiceId" element={<TheInvoice />} />
-                    <Route path="/storepage" element={<Store />} />
-                    <Route path="/ProductAndPriceData/:productId" element={<ProductAndPriceData />} />
+            <Route path="/Addprescription" element={<Addprescription />} />
+            <Route path="/AddProduct" element={<AddProduct />} />
+            <Route path="/theInvoice/:ivoiceId" element={<TheInvoice />} />
+            <Route path="/storepage" element={<Store />} />
+            <Route path="/ProductAndPriceData/:productId" element={<ProductAndPriceData />} />
             <Route path="/PharmacrPage" element={<PharmacrPage />} />
             <Route path="/" element={<Homepage />} />
             <Route path="/Reception" element={<Reception />} />
@@ -58,7 +58,8 @@ function App() {
             <Route path="/userList" element={<UserList />} />
             <Route path='drugstore' element={<div><Outlet /></div>}>
               <Route path='requests' element={<RequestPage />} />
-              <Route path=':requestId' element={<h1>Hello</h1>}/>
+              <Route path='request-response' element={<ResponsePage />} />
+              <Route path=':requestId' element={<h1>why this ?</h1>} />
             </Route>
           </Route>
         </Routes>
