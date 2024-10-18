8d79bedb63d5c668714602205ce5ecf4aad04d76
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-18 23:55:36 +0300

    nothing changed

--- src/pages/DrugStore/RequestResponse/ResponsePage.jsx
+++ src/pages/DrugStore/RequestResponse/ResponsePage.jsx
@@ -1,7 +1,9 @@
 import RequestResponse from "./RequestResponse";
 import RequestResponseHeader from "./RequestResponseHeader";
+import { useEffect } from "react";
 
 const ResponsePage = () => {
+  
   return (
     <div className="w-[85%] mx-auto">
       <RequestResponseHeader/>
