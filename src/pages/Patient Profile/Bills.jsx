bdf92f615b46515a9611a04494445945d241aa0f
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-18 23:57:27 +0300

    display invoices, prescriptions for patient

--- src/pages/Patient Profile/Bills.jsx
+++ src/pages/Patient Profile/Bills.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import './cover.css';
+import { Link, useNavigate } from 'react-router-dom';
 const Bills = ({ invoices }) => {
+  // counter to display the number of invoices
+  let counter = 0;
+
   return (
     <div className="mt-4">
       {/* Header */}
@@ -12,21 +17,32 @@ const Bills = ({ invoices }) => {
 
       {/* Scrollable Container */}
       <div className="mt-2 max-h-[400px] overflow-y-scroll rounded-md pb-24 scroll-bar">
-        {invoices.map((data, index) => (
-          <Bill key={index} patientBill={data} />
-        ))}
+        {
+          invoices.map((data) => (
+            data.items.map((invoice) => {
+              counter += 1; // Increment the counter for each invoice item
+              return <Bill key={counter} patintDetails={data} patientBill={invoice} invoiceNumber={counter} />;
+            })
+          ))
+        }
       </div>
     </div>
   );
 };
 
-const Bill = ({ patientBill }) => {
+const Bill = ({ patientBill, patintDetails, invoiceNumber }) => {
+  const navigate = useNavigate();
+
+  const handleShowInvoice = () => {
+    navigate(`/theInvoice/${patintDetails._id}`)
+  }
   return (
-    <div className="grid grid-cols-4 text-sm sm:text-xl place-items-center sm:place-items-end p-2 sm:p-4 bg-[#F5F5F5] mt-2 mb-5 border rounded-md gap-2 sm:gap-5">
-      <p>{patientBill.invoiceDate}</p>
-      <p>{patientBill.status}</p>
-      <p>{patientBill.patientName}</p>
-      <p>{patientBill.invoiceNumber}</p>
+    <div className="grid grid-cols-4 text-sm sm:text-xl place-items-center sm:place-items-end p-2 sm:p-4 bg-[#F5F5F5] mt-2 mb-5 border rounded-md gap-2 sm:gap-5" onDoubleClick={() => handleShowInvoice()}>
+      <p>{new Date(patintDetails.invoiceDate).toLocaleDateString('en-CA')}</p>
+      {/* TODO:here suppose to be patinetBill.status OR removed */}
+      <p>مكتمل</p>
+      <p>{patintDetails.patientName}</p>
+      <p>{invoiceNumber}</p>
     </div>
   );
 };
--- src/pages/Patient Profile/MedicalPrescriptions.jsx
+++ src/pages/Patient Profile/MedicalPrescriptions.jsx
@@ -1,38 +1,47 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import add_icon from '../../images/add_icon.svg'
 import './cover.css';
+import { useState } from 'react';
 
-const MedicalPrescriptions = ({ medicalRecords }) => {
+const MedicalPrescriptions = ({ prescriptions }) => {
+  const { patientId } = useParams();
+  let counter = 0;
+
+
+
   return (
     <div className="mt-4">
       {/* Header */}
       <div className="grid grid-cols-5 place-items-center gap-2 sm:gap-5 p-2 bg-[#F5F5F5] rounded-md text-sm sm:text-2xl 2xl:text-3xl">
-        <Link to='/add-prescription'>
+        <Link to={`/add-prescription/${patientId}`}>
           <img src={add_icon} alt="add prescription" />
         </Link>
         <p>تاريخ الوصفة</p>
         <p>أسم الطبيب</p>
-        <p>أسم المريض</p>
+        <p>اسم الدواء</p>
         <p>رقم الوصفة</p>
       </div>
 
       {/* Scrollable Container */}
       <div className="mt-2 max-h-[400px] overflow-y-scroll rounded-md pb-24 scroll-bar">
-        {medicalRecords.map((data, index) => (
-          <Bill key={index} patientRecord={data} />
-        ))}
+        {prescriptions.map((prescription_details, index) => (
+          prescription_details.prescriptions.map((item, id) => {
+            counter += 1;
+            return <Bill key={id} prescription_details={prescription_details} medicineGiven={item} prescriptionNumber={counter} />
+          }
+          )))}
       </div>
     </div>
   );
 };
 
-const Bill = ({ patientRecord }) => {
+const Bill = ({ prescription_details, medicineGiven, prescriptionNumber }) => {
   return (
     <div className="grid grid-cols-4 text-sm sm:text-xl place-items-center sm:place-items-end p-2 sm:p-4 bg-[#F5F5F5] mt-2 mb-5 border rounded-md gap-2 sm:gap-5 text-right">
-      <p>{patientRecord.prescriptionDate}</p>
-      <p>{patientRecord.doctorName}</p>
-      <p>{patientRecord.patientName}</p>
-      <p className='justify-self-end'>{patientRecord.prescriptionNumber}</p>
+      <p>{new Date(prescription_details.reviewDate).toLocaleDateString('en-CA')}</p>
+      <p>{prescription_details.doctorName}</p>
+      <p>{medicineGiven.medicineName}</p>
+      <p className='justify-self-end'>{prescriptionNumber}</p>
     </div>
   );
 };
