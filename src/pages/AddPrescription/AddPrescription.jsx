660a1462b2016f736aade445acb2d834c2699c1d
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-18 23:46:37 +0300

    fethcing user data and fixing prescriptin form

--- src/pages/AddPrescription/AddPrescription.jsx
+++ src/pages/AddPrescription/AddPrescription.jsx
@@ -1,14 +1,38 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import FormActions from "./FormActions";
 import PrescriptionForm from "./PrescriptionForm";
 import PrescriptionHeader from "./PrescriptionHeader";
+import { useParams } from "react-router";
+import axios from "axios";
 
 const AddPrescription = () => {
+  const { patientId } = useParams();
+  const [patientData, setPatientData] = useState({});
+
+  useEffect(() => {
+    const getPatient = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        const respone = await axios.get(`http://localhost:4000/api/patient/patients/${patientId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        const data = await respone.data;
+        setPatientData(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getPatient();
+  }, [])
+
   return (
     <div className="w-[85%] mx-auto mb-4">
       <PrescriptionHeader />
       <main className="w-full h-fit mt-4  rounded-lg">
-        <PrescriptionForm />
+        <PrescriptionForm patientData={patientData} />
       </main>
     </div>
   );
