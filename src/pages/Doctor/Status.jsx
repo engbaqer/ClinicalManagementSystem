2adfee3dcefc450c0fb67aa1b1076d500afaaf38
Ahmed Alharees <ahmedjoad.2014@gmail.com>
2024-10-18 23:50:40 +0300

    when doctor click check patient is in entered list

--- src/pages/Doctor/Status.jsx
+++ src/pages/Doctor/Status.jsx
@@ -1,12 +1,31 @@
 import wait_icon from '../../images/wait.svg';
 import check_icon from '../../images/check.svg';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useContext } from 'react';
+import { ClinicalContext } from '../auth/contextFile';
 
 const Status = ({ subscriptionStatus, patientId }) => {
+  const { token } = useContext(ClinicalContext);
   const navigate = useNavigate();
 
   const goToPatientProfile = () => {
+    // before going to the patient profile move patient to doctor
+    movePatientToDoctor();
     navigate(`/patient-profile/${patientId}`);
+  }
+
+  const movePatientToDoctor = async () => {
+    try {
+      const response = await axios.put('http://localhost:4000/api/doctor/movePatientToDoctor', { patientId }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
